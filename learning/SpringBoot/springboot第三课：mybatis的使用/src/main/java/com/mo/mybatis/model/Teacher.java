43a965ce8f1854a43a336a820439f801f3cd4660
mwnzb08 <moqiue0729@163.com>
2019-12-29 23:37:48 +0800

    mybatis/dao/StudentMapper.java

---
+++ learning/SpringBoot/springboot第三课：mybatis的使用/src/main/java/com/mo/mybatis/model/Teacher.java
@@ -0,0 +1,43 @@
+package com.mo.mybatis.model;
+
+public class Teacher {
+    private Long id;
+
+    private String classes;
+
+    private String name;
+
+    private String sex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes == null ? null : classes.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+}
